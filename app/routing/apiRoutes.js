2dfd573b6818e15850ec3250946dfe9504d6e949
michael mason <michael@michaeljmason.com>
2017-09-12 14:55:16 -0700

    changes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,4 +1,4 @@
-var friendData 		= require('../data/friends.js');
+var friends 		= require('../data/friends.js');
 var path 			= require('path');
 
 // API GET Requests - when users "visit" a page. 
@@ -17,7 +17,11 @@ module.exports = function(app){
 
 
 	app.post('/api/friends', function(req, res){
-
+        // console.log(friends);
+        // console.log('----------------------------------------');
+        // console.log(req.body)
+        // console.log('----------------------------------------');
+        // console.log([friends].length);
 		var greatMatch = {
 			name: "",
 			image: "",
@@ -31,8 +35,8 @@ module.exports = function(app){
 		var totalDifference = 0;
 
 		//loop through the friends data array of objects to get each friends scores
-		for(var i = 0; i < [friends].length-1; i++){
-			console.log(friends[i].name);
+		for(var i = 0; i < friends.length-1; i++){
+			// console.log(friends[i].name);
 			totalDifference = 0;
 
 			//loop through that friends score and the users score and calculate the 
@@ -40,8 +44,10 @@ module.exports = function(app){
 			for(var j = 0; j < 10; j++){
 				// We calculate the difference between the scores and sum them into the totalDifference
 				totalDifference += Math.abs(parseInt(usrScores[j]) - parseInt(friends[i].scores[j]));
-				// If the sum of differences is less then the differences of the current "best match"
-				if (totalDifference <= greatMatch.friendDifference){
+                // If the sum of differences is less then the differences of the current "best match"
+                
+                // if (j === 9) console.log(totalDifference);
+				if (totalDifference <= greatMatch.matchDifference){
 
 					// Reset the bestMatch to be the new friend. 
 					greatMatch.name = friends[i].name;
@@ -50,7 +56,9 @@ module.exports = function(app){
 				}
 			}
 		}
-
+        console.log('----------------------------------------');
+        console.log(usrData);
+        console.log('----------------------------------------');
 		friends.push(usrData);
  
 		res.json(greatMatch);
